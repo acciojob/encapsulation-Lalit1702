4e5cf21352d2ff8f1fb0257240d9691639d17e1a
Lalit Patil <lalitbansipatil1702@gmail.com>
2024-05-19 23:17:06 +0530

    Encapsulation done

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,8 @@
 package com.driver;
-
 public class Main {
-  
+  public static void main(String[] args) {
+    RWOnly read = new RWOnly();
+    read.setName("Lalit");
+    System.out.print(read.getName());
+  }
 }
